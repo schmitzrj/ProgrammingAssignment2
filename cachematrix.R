6dcb5fe0b688b5fd78d35dfa63432af792ec4d06
Ryan Schmitz <jacquesdereims@gmail.com>
2017-04-20 23:20:36 -0700

    added code to assignment file
    the code has been added to the assignment file to make the functions
    work as described in scoping documents

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions deal with creating a special matrix object and then using 
+## that object to return the inverse of the matrix either from cache if available 
+## or from calculation if necessary.
 
-## Write a short comment describing this function
+## Creates a matrix with a variable for its own inverse
+## This function is able to retrieve a chached inverse or create on if empty.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+    
+  setInverse <- function(solve) inv <<- solve
+  
+  getInverse <- function() inv
+  
+  list(set = set, get = get, 
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Retrieves the inverse of the special matrix if it is already cached, 
+## otherwise calculate the inverse and return it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  
+  if(!is.null(inv)) {
+    message("getting matrix inverse")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
 }
